db11a4d1d2c39401d936b7ddd338c7dde82ff59d
Asma CHKIRIDA <asmachkirida02@gmail.com>
2024-08-09 10:54:58 +0100

    drivers page - display

--- src/app/admin/drivers/drivers.component.ts
+++ src/app/admin/drivers/drivers.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-drivers',
   templateUrl: './drivers.component.html',
-  styleUrl: './drivers.component.css'
+  styleUrls: ['./drivers.component.css']
 })
-export class DriversComponent {
+export class DriversComponent implements OnInit {
+  drivers: any[] = [];
+  currentPage = 1;
+  pageSize = 6;
+  totalPages = 1;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadDrivers();
+  }
+
+  loadDrivers() {
+    this.http.get<any[]>(`http://localhost:8080/admin/drivers`).subscribe(data => {
+      this.totalPages = Math.ceil(data.length / this.pageSize);
+      this.drivers = data.slice((this.currentPage - 1) * this.pageSize, this.currentPage * this.pageSize);
+    });
+  }
+
+  editDriver(id: number) {
+    // Implement edit logic
+    console.log('Edit driver', id);
+  }
+
+  deleteDriver(id: number) {
+    // Implement delete logic
+    console.log('Delete driver', id);
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.loadDrivers();
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.loadDrivers();
+    }
+  }
 }
