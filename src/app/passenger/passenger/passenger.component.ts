d178a94273b1a13bf2b0ef22f8b1c48cc76ab452
Asma CHKIRIDA <asmachkirida02@gmail.com>
2024-08-29 21:19:03 +0100

    Logout passenger

--- src/app/passenger/passenger/passenger.component.ts
+++ src/app/passenger/passenger/passenger.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { BikeRideDialogComponent } from '../bike-ride-dialog/bike-ride-dialog.component';
+import { AuthService } from '../../services/auth.service'; // Adjust the path if necessary
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-passenger',
@@ -9,11 +11,16 @@ import { BikeRideDialogComponent } from '../bike-ride-dialog/bike-ride-dialog.co
 })
 export class PassengerComponent {
 
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog, private authService: AuthService, private router: Router) {}
 
   openBikeRideModal(): void {
     this.dialog.open(BikeRideDialogComponent, {
       width: '400px', // Adjust the width as needed
     });
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/signin']); // Adjust the path to your sign-in route
   }
 }
