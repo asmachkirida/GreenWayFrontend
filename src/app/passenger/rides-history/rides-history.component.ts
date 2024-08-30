123d1cc3e8a37bdf36f2ec7ec51e7a9763d26ddc
Asma CHKIRIDA <asmachkirida02@gmail.com>
2024-08-30 22:27:33 +0100

    rating a driver

--- src/app/passenger/rides-history/rides-history.component.ts
+++ src/app/passenger/rides-history/rides-history.component.ts
@@ -11,21 +11,45 @@ export class RidesHistoryComponent implements OnInit {
   reviewDetails: string = '';
   selectedRideId: number | null = null;
   showAddReviewModal: boolean = false;
+  showUpdateRatingModal: boolean = false;
+  selectedDriverId: number | null = null;
+  newRating: number | null = null;
 
   constructor(private http: HttpClient) {}
-
   ngOnInit(): void {
     const userId = localStorage.getItem('userId');
     this.http.get<any[]>(`http://localhost:8080/rides/passenger/${userId}`).subscribe(
       (response) => {
         this.rides = response;
+        
+        // Fetch driver ID for each ride based on carId
+        this.rides.forEach(ride => {
+          this.getDriverIdByCarId(ride.carId);
+        });
       },
       (error) => {
         console.error('Error fetching rides history:', error);
       }
     );
   }
+  
 
+  private getDriverIdByCarId(carId: number): void {
+    this.http.get<any>(`http://localhost:8080/driver/cars/${carId}`).subscribe(
+      (response) => {
+        // Update rides with driverId
+        this.rides.forEach(ride => {
+          if (ride.carId === carId) {
+            ride.driverId = response.driverId; // Add driverId to the ride
+          }
+        });
+      },
+      (error) => {
+        console.error('Error fetching driver by carId:', error);
+      }
+    );
+  }
+  
   openAddReviewPopup(rideId: number): void {
     this.selectedRideId = rideId;
     this.showAddReviewModal = true;
@@ -49,6 +73,41 @@ export class RidesHistoryComponent implements OnInit {
         },
         (error) => {
           console.error('Error submitting review:', error);
+        }
+      );
+    }
+  }
+
+  openUpdateRatingModal(driverId: number): void {
+    this.selectedDriverId = driverId;
+    this.showUpdateRatingModal = true;
+  }
+
+  closeUpdateRatingModal(): void {
+    this.showUpdateRatingModal = false;
+    this.newRating = null;
+  }
+
+  submitRating(): void {
+    console.log("triggered");
+    console.log(this.selectedDriverId);
+    console.log(this.newRating);
+    if (this.selectedDriverId != null && this.newRating != null) {
+      console.log("triggered x2x2x2x2");
+      console.log(this.selectedDriverId);
+      console.log(this.newRating);
+
+      this.http.put(`http://localhost:8080/admin/${this.selectedDriverId}/rating`, null, {
+        params: { newRating: this.newRating.toString() }
+      }).subscribe(
+        () => {
+          alert('Rating updated successfully');
+          this.closeUpdateRatingModal();
+          this.ngOnInit(); // Refresh the rides history
+        },
+        (error) => {
+          alert('Error updating rating');
+          console.error(error);
         }
       );
     }
