e7e5ad7457bcff446334c41738e88845a0acd9cb
Asma CHKIRIDA <asmachkirida02@gmail.com>
2024-08-31 21:19:56 +0100

    left pane design

--- src/app/available-bike-rides/available-bike-rides.component.ts
+++ src/app/available-bike-rides/available-bike-rides.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-available-bike-rides',
   templateUrl: './available-bike-rides.component.html',
-  styleUrl: './available-bike-rides.component.css'
+  styleUrls: ['./available-bike-rides.component.css']
 })
-export class AvailableBikeRidesComponent {
+export class AvailableBikeRidesComponent implements OnInit {
+  bikeRides: any[] = [];
+  userId: number | null = null;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.userId = this.getUserIdFromLocalStorage();
+    if (this.userId !== null) {
+      this.fetchAvailableBikeRides();
+    } else {
+      console.error('User ID is not set in local storage.');
+    }
+  }
+
+  getUserIdFromLocalStorage(): number | null {
+    const userId = localStorage.getItem('userId');
+    return userId ? parseInt(userId, 10) : null;
+  }
+
+  fetchAvailableBikeRides(): void {
+    this.http.get<any[]>('http://localhost:8080/bike-rides')
+      .subscribe(data => {
+        // Filter out bike rides created by the current user
+        this.bikeRides = data.filter(ride => ride.creatorId !== this.userId);
+      });
+  }
+
+  participateInBikeRide(bikeRideId: number): void {
+    if (this.userId !== null) {
+      this.http.post(`http://localhost:8080/bike-rides/${bikeRideId}/participants/${this.userId}`, {})
+        .subscribe({
+          next: () => {
+            alert('Successfully joined the bike ride!');
+            // Optionally, you can refresh the list or update the UI here
+          },
+          error: (err) => {
+            console.error('Error joining bike ride', err);
+          }
+        });
+    }
+  }
 }
