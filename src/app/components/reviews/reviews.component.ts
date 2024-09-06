8adc7b5ac49f03f06f5ee487cb17766e3b931135
Asma CHKIRIDA <asmachkirida02@gmail.com>
2024-09-06 20:00:04 +0100

    infinite carousel -v2

--- src/app/components/reviews/reviews.component.ts
+++ src/app/components/reviews/reviews.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-reviews',
   templateUrl: './reviews.component.html',
   styleUrls: ['./reviews.component.css']
 })
-export class ReviewsComponent {
-  reviews = [
-    { text: "Amazing service, I loved the bike ride experience!", user: "Jane Doe" },
-    { text: "Great app, easy to use and very helpful.", user: "John Smith" },
-    // Add more reviews as needed
-  ];
+export class ReviewsComponent implements OnInit {
+  reviews: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.fetchReviews();
+  }
+
+  fetchReviews(): void {
+    this.http.get('http://localhost:8080/passenger/reviews').subscribe((reviews: any) => {
+      reviews.forEach((review: any) => {
+        this.http.get(`http://localhost:8080/rides/${review.rideId}`).subscribe((ride: any) => {
+          // We use "Green Way User" instead of the actual passenger name
+          this.reviews.push({
+            details: review.details,
+            userName: 'Green Way User'
+          });
+        });
+      });
+    });
+  }
 }
